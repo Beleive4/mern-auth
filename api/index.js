c31bff58f451298dc5e73397177c8b58fb50aaac
Kiran <kiran@intellispiders.com>
2024-03-07 12:50:19 +0530

    updated code

--- api/index.js
+++ api/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected!'))
+    .catch((err) => {
+        console.log(err, "ERROR");
+    })
+
+
+
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello Kiran.")
 })
 
