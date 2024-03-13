8c530dab2c0ac961aa2df24352dc63da19082799
Kiran <kiran@intellispiders.com>
2024-03-13 15:31:35 +0530

    cursor pointer updated code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -17,6 +17,7 @@ export default function Header() {
                     </Link>
 
                     <li
+                        style={{ cursor: 'pointer' }}
                         onClick={() => {
                             console.log("Hello");
                             localStorage.clear();
