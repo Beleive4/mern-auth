c60d8d5aa6cb0fefc5d5fd04ba7d42d11fb928d0
Kiran <kiran@intellispiders.com>
2024-03-14 11:57:30 +0530

    upload image to firebase with percentage loader

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,10 +1,49 @@
 import React, { useRef } from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase'
+
 
 export default function About() {
   const fileRef = useRef(null);
   const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
+  const [imageError, setImageError] = useState(null);
 
+
+  useEffect(() => {
+    if (image) {
+      handleFileUpload(image);
+    }
+  }, [image])
+
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred /
+          snapshot.totalBytes
+        ) * 100;
+        setImagePercent(Math.round(progress));
+        console.log(Math.round(progress), "progress");
+      },
+
+      (error) => {
+        setImageError(true)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then
+          ((downloadUrl) => {
+            console.log(downloadUrl, "downloadUrl");
+          })
+
+      });
+  }
 
   return (
     <div>
@@ -14,8 +53,9 @@ export default function About() {
 
         <input type="file" ref={fileRef} accept='image/*'
           onChange={(e) => {
+            setImageError(null);
             setImage(e.target.files[0]);
-            console.log(e.target.files[0],"kkkk4");
+            console.log(e.target.files[0], "kkkk4");
           }}
         />
 
@@ -26,6 +66,21 @@ export default function About() {
           rounded-full object-cover mt-2'
           onClick={() => fileRef.current.click()}
         />
+        <p>
+          {imageError ? (
+            <>
+              <span className='text-red-700'>Error uploading image</span>
+            </>
+          ) : (
+            <>
+              {imagePercent > 0 && imagePercent < 100 ? (
+                <span className='text-slate-700'>{`Uploading... ${imagePercent}%`}</span>
+              ) : imagePercent === 100 ? (
+                <span className='text-green-700'>Image uploaded successfully</span>
+              ) : null}
+            </>
+          )}
+        </p>
 
       </form>
 
